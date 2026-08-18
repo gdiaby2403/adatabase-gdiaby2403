939985ab2b4b9970ffcbd12b1933aa753f8b51c7
Goundoba Diaby <goundoba.diaby2403@gmail.com>
2026-08-18 09:50:57 +0200

    ajout type  enum migration_up

--- migration_up.sql
+++ migration_up.sql
@@ -1,1 +1,8 @@
-CREATE TYPE
+CREATE TYPE etat_arrivee AS ENUM ('bon etat', 'a reparer', 'hors-service')
+CREATE TYPE statut_objet AS ENUM ('arrivee', 'en reparation', 'en rayon', 'vendu', 'recyclé')
+CREATE TYPE type_depot AS ENUM ('depot en boutique', 'collecte a domicile')
+CREATE TYPE mode_paiement AS ENUM ('especes', 'carte', 'cheque')
+CREATE TYPE resultat AS ENUM ('reussi', 'echouer')
+
+
+

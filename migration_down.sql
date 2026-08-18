a538a7c463a6cb3ae58b2a86502a51bda504c998
Goundoba Diaby <goundoba.diaby2403@gmail.com>
2026-08-18 15:04:43 +0200

    ajout fichiers  migrations tables et types

---
+++ migration_down.sql
@@ -0,0 +1,32 @@
+DROP TYPE IF EXISTS type_depot;
+DROP TYPE IF EXISTS etat_arrivee;
+DROP TYPE IF EXISTS statut_objet;
+DROP TYPE IF EXISTS resultat_reparation;
+DROP TYPE IF EXISTS mode_paiement;
+
+DROP TABLE IF EXISTS reparation;
+
+DROP TABLE IF EXISTS incription;
+
+DROP TABLE IF EXISTS objet;
+
+DROP TABLE IF EXISTS competence_benevole;
+
+DROP TABLE IF EXISTS atelier;
+
+DROP TABLE IF EXISTS depot;
+
+DROP TABLE IF EXISTS categorie;
+
+DROP TABLE IF EXISTS vente;
+
+DROP TABLE IF EXISTS competence;
+
+DROP TABLE IF EXISTS benevole;
+
+DROP TABLE IF EXISTS personne;
+
+
+
+
+
